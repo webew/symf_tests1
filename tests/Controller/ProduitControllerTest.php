c5a823d7dac007081f2aba1168d64588b72ab7bf
webew <fillion.bertrand@gmail.com>
2019-11-19 23:27:08 +0100

    recup données

--- tests/Controller/ProduitControllerTest.php
+++ tests/Controller/ProduitControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Produit;
+use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProduitControllerTest extends WebTestCase
@@ -10,13 +12,33 @@ class ProduitControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $client->followRedirects();
+
+        $this->em = static::$container->get("doctrine")->getManager();//récupération de l'entity manager
+        //outil de gestion de la bdd
+        $schemaTool = new SchemaTool($this->em);
+        //suppression de la bdd
+        $schemaTool->dropDatabase();
+        //création de la bdd
+        static $metadata = null;
+        if(! $metadata){
+            $metadata = $this->em->getMetadataFactory()->getAllMetadata();
+        }
+        $schemaTool->createSchema($metadata);
+        //navigation vers l'url du formulaire
         $crawler = $client->request('GET', '/produit/new');
+        //soumission du formulaire
         $buttonCrawlerNode = $crawler->selectButton('Save');
-        $form = $buttonCrawlerNode->form(['produit[name]'=>'toto']);
+        $form = $buttonCrawlerNode->form(['produit[name]'=>'tata']);
         $client->submit($form);
 
+//        $em = static::$container->get("doctrine")->getManager();//récupération de l'entity manager
+        //récupération des produits
+        $produits = static::$container->get("doctrine")->getRepository(Produit::class)->findAll();//récupération de l'entity manager
+//        dd($produits);
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Produit index');
-        $this->assertSelectorTextContains('td', 'toto');
+        $this->assertCount(1, $produits);
     }
+
+
 }
